5677ad8f54a5fc29e67ce0a1e15283529fc1c03a
JamesKo1026 <jamesko1026@gmail.com>
2016-12-11 16:59:26 +0800

    Add document_type and translator4_id into document table

---
+++ database/migrations/2016_12_11_085251_add_document_type_and_translator4_id.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDocumentTypeAndTranslator4Id extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->unsignedInteger('translator4_id');
+            $table->foreign('translator4_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->unsignedInteger('document_type')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->dropColumn('translator4_id');
+            $table->dropColumn('document_type');
+        });
+    }
+}
