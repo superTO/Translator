beef75c3726ba81dee2872c6fde828e528ccd7bd
DoYouBelieveJesus <gg4627942@gmail.com>
2016-12-19 00:01:26 +0800

    could register new account

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,6 +49,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|max:255',
+            'account' => 'required',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -64,8 +65,12 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'account' => $data['account'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'role' => 1,
+            'phone_number' => $data['phone'],
+            'ssn'=>$data['id_number']
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role',
+        'name', 'email', 'password', 'role', 'account','phone_number','ssn'
     ];
     /**
      * The attributes that should be hidden for arrays.
