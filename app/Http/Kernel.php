e68a85c7e87598272b2d2a4f81a6d68d66970470
az25256058 <az25256058@gmail.com>
2016-12-23 03:09:46 +0800

    modify middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,10 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\AdminCheck::class,
+        'pm' => \App\Http\Middleware\PMCheck::class,
+        'user' => \App\Http\Middleware\UserCheck::class,
+        'trans' => \App\Http\Middleware\TransCheck::class,
+
     ];
 }
---
+++ app/Http/Middleware/PMCheck.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+
+class PMCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        if($user->role != 2){
+            return redirect('/home');
+        }
+        return $next($request);
+    }
+}
