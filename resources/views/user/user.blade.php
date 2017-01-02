7866f68cea7ad7da5df4aa6e68eea31a3569be5e
Верный  俺の嫁 <  >
2017-01-02 23:04:58 +0800

    modify user search

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="row">
                     <div class="col-md-4 col-md-offset-3">
-                        <form  action="/user/index_" method="get" class="search-form" role="search" >
+                        <form  action="/user_" method="get" class="search-form" role="search" >
                             <div class="form-group has-feedback">
                                 <input type="text" class="form-control" name="search" id="search" placeholder="search">
                                 <span class="glyphicon glyphicon-search form-control-feedback"></span>
